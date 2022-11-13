e8b556c0218734b1e2a4d75319fcb178df0703e9
yang <wuzhikun526@163.com>
2022-11-13 23:19:56 +0800

    comment：pick & firstOfArray

--- 0002-pick/template.ts
+++ 0002-pick/template.ts
@@ -1,3 +1,7 @@
+/**
+ * 1. keyof T 限制类型都是T的键，联合类型
+ * 2. [P in K] 取出所有键
+ */
 type MyPick<T, K extends keyof T> = {
   [P in K]: T[P]
 }
--- 0004-first-of-array/template.ts
+++ 0004-first-of-array/template.ts
@@ -1,3 +1,7 @@
+/**
+ * 1. infer F 声明变量F表示第一个元素
+ * 2. 条件类型取出第一个元素
+ */
 // type First<T extends any[]> = T["length"] extends 0 ? never : T[0]
 // type First<T extends any[]> = T extends [] ? never : T[0]
 type First<T extends any[]> = T extends [infer F, ...any[]] ? F : never
