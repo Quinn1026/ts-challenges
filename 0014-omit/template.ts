3e42f9d5d18abbaea8405f4edea33d551c01bfd5
yang <wuzhikun526@163.com>
2022-11-16 17:07:46 +0800

    chore：update omit

--- 0014-omit/template.ts
+++ 0014-omit/template.ts
@@ -2,6 +2,7 @@
  * 1. 返回{}
  * 2. Exclude排除联合类型K
  */
-type MyOmit<T, K extends keyof T> = {
-  [P in Exclude<keyof T, K>]: T[P]
-}
+type MyOmit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+// type MyOmit<T, K extends keyof T> = {
+//   [P in Exclude<keyof T, K>]: T[P]
+// }
