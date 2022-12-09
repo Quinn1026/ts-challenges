c231ec6b1eeb226964dc4a19d3a07d9d4b0fc6b4
yang <wuzhikun526@163.com>
2022-12-09 23:35:30 +0800

    perf：inorderTraversal

--- 0062-InorderTraversal/template.ts
+++ 0062-InorderTraversal/template.ts
@@ -6,8 +6,9 @@ interface TreeNode {
 /**
  * 1. 二叉树中序遍历
  * 2. 递归
+ * 3. T extends TreeNode (实例化过深?) --优化--> [T] extends [TreeNode] ？why
  */
-type InorderTraversal<T extends TreeNode | null> = T extends TreeNode
+type InorderTraversal<T extends TreeNode | null> = [T] extends [TreeNode]
   ? [
       ...T['left'] extends TreeNode ? InorderTraversal<T['left']> : [],
       T['val'],
